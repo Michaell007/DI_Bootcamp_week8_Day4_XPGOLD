a7d5632f2573249f7057736e827d7955cab6cdd0
Michael Yapi .A <michaelakichi@gmail.com>
2023-02-18 18:13:56 +0000

    Update exercice2.php

--- exercice2.php
+++ exercice2.php
@@ -1,33 +1,30 @@
-<?php 
-// 1- Créez une connexion de base de données au serveur PostgreSQL.
-$host = 'your_host_name';
-$dbname = 'your_database_name';
-$user = 'your_username';
-$password = 'your_password';
+<?php
+  
+    $dbhost = 'localhost';
+    $dbport = '5432';
+    $dbname = 'php_postgre';
+    $dbuser = 'postgres';
+    $dbpassword = 12136270;
 
-try {
-    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully\n";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+    $dbconn = pg_connect("host=$dbhost port=$dbport dbname=$dbname user=$dbuser password=$dbpassword");
 
+    if (!$dbconn) {
+        echo "Could not connect to database.\n";
+    exit;
+    }else{
+        echo "Connected to database.\n";
+    }
 
-// 2- Créez une requête SQL.
-$sql = "DELETE FROM COMPANY WHERE ID = 2";
+    $query = "DELETE from COMPANY where ID = 2;";
 
+    $result = pg_query($dbconn, $query);
 
-// 3- Exécuter une requête pour supprimer des lignes de la table
-$conn->exec($sql);
+    if (!$result) {
+       echo "Query failed.\n";
+    exit;
+    }else{
+        echo "Query succeeded.\n";
+    }
 
-//  Récupérez et affichez les enregistrements restants de la COMPANYtable à l'aide d'une instruction SELECT 
-$sql = "SELECT * FROM COMPANY";
-$stmt = $conn->query($sql);
-while ($row = $stmt->fetch()) {
-    echo "ID: " . $row['ID'] . ", Name: " . $row['NAME'] . ", Age: " . $row['AGE'] . ", Salary: " . $row['SALARY'] . "\n";
-}
- // 4- Fermez la connexion à la base de données
- $conn = null;
-
+    pg_close($dbconn);
 ?>
